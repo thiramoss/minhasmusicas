754a8c6070a843038b43845707d74ba9105d7d9b
thiramoss <trs.tiba@gmail.com>
2024-06-13 22:01:35 -0300

    Primeiro commit

---
+++ src/br/com/thiagoramos/minhasmusicas/modelos/Podcast.java
@@ -0,0 +1,22 @@
+package br.com.thiagoramos.minhasmusicas.modelos;
+
+public class Podcast extends {
+    private String apresentador;
+    private String descricao;
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
